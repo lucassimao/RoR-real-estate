af4cd734bf12b6822eef91aacee2a351c355a173
Lucas Simão <lucas.simao@trt22.jus.br>
2020-01-08 16:53:36 -0300

    generating the HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+    root to: 'home#index'
   resources :images
   resources :properties
   resources :landlords
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
