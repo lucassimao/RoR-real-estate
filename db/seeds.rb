7eb11d4f3f82273f6fc2f7aaa169fa75f26074f1
Lucas Simão <lucas.simao@trt22.jus.br>
2020-01-09 10:33:59 -0300

    creating seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |n|
+    Landlord.create!(name: "Landlord #{n}")
+end
+
+100.times do |n|
+    property = Property.new
+    random_type = Property.property_types.keys.sample(1).first
+    random_text = (0...100).map { ('a'..'z').to_a[rand(26)] }.join
+    
+    property.property_type = random_type
+    property.description = "Property #{n} type #{random_type} #{random_text}"
+    property.width = rand(10..30)
+    property.length = rand(10..50)
+    property.price = rand(100.0 .. 1000000.0)
+    property.landlord = Landlord.find(rand(1..50))
+    property.save
+end
